f217c603092acf99000eac04b4c8997d2bd1fdca
Jakob <jako2889@stud.kea.dk>
2019-02-07 12:48:06 +0100

    tilføjet javascript scafolding

--- studentlist.js
+++ studentlist.js
@@ -5,6 +5,31 @@ window.addEventListener("DOMContentLoaded", init);
 function init() {
   console.log("init");
 
-  // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+  getJson();
 
+  // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
+}
+function getJson() {
+  console.log("getJson");
+
+  // NOTE: Maybe also call SortbyFirst the first time... Investigate
+  filterList();
+}
+
+function filterList() {
+  console.log("filterList");
+
+  displayList();
+}
+
+function displayList() {
+  console.log("displayList");
+}
+
+function clickSortByFirst() {
+  console.log("clickSortByFirst");
+}
+
+function sortByFirst() {
+  console.log("sortByFirst");
 }
