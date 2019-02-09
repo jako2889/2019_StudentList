20bffc7edcc810b7bdce27cd2d5f8e7e7152b5a1
Jakob <jako2889@stud.kea.dk>
2019-02-09 15:30:01 +0100

    tilføjet filter

--- studentlist.js
+++ studentlist.js
@@ -2,6 +2,13 @@
 
 window.addEventListener("DOMContentLoaded", init);
 
+//GLOBALE VARIABLER -------------------------------
+
+let allStudents;
+let postTarget = document.querySelector(".target");
+let postOutput = document.querySelector(".output");
+let house = "alle";
+
 function init() {
   console.log("init");
 
@@ -9,11 +16,32 @@ function init() {
 
   // TODO: Load JSON, create clones, build list, add event listeners, show modal, find images, and other stuff ...
 }
-function getJson() {
+
+async function getJson() {
   console.log("getJson");
+
+  let jsonObject = await fetch("http://petlatkea.dk/2019/students1991.json");
+
+  allStudents = await jsonObject.json();
+
+  document.querySelectorAll(".filter_knap").forEach(knap => {
+    knap.addEventListener("click", filter);
+  });
+
+  console.log(allStudents);
 
   // NOTE: Maybe also call SortbyFirst the first time... Investigate
   filterList();
+}
+// Få data fra knap og send det videre til display
+function filter() {
+  house = this.getAttribute("data-house");
+
+  console.log(house);
+
+  postOutput.textContent = "";
+
+  displayList();
 }
 
 function filterList() {
@@ -21,9 +49,35 @@ function filterList() {
 
   displayList();
 }
-
+// Her vises eleverne i et loop (for each)
 function displayList() {
   console.log("displayList");
+
+  allStudents.forEach(student => {
+    console.log("elsker mit liv");
+
+    //filter udskriv ---------------------------------------
+
+    if (house == "alle") {
+      printPost(student);
+    }
+
+    if (house == student.house) {
+      printPost(student);
+      console.log("det virker");
+    }
+
+    //Udskriver fra template til output
+
+    function printPost(student) {
+      let klon = postTarget.cloneNode(true).content;
+
+      klon.querySelector(".name").textContent = student.fullname;
+      klon.querySelector(".house").textContent = student.house;
+
+      postOutput.appendChild(klon);
+    }
+  });
 }
 
 function clickSortByFirst() {
